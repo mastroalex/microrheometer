d179b56643ba5a7d2eb4c029fa7841138829f8f7
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-06-15 12:53:47 +0200

    code

--- code/main.m
+++ code/main.m
@@ -1,9 +1,10 @@
 clear all
 close all
 clc
-
+% add path for subroutines in subfolder
 addpath('subroutine/');
 
+% load material parameters
 properties_parameters
 
 % select the input waveform by uncomment the flag
@@ -16,6 +17,7 @@ flag='harmonic';
 % omega=2*pi*f;
 % parameters=[k_0,k_1,gamma_0,gamma_1,F_bar,tau,omega];
 
+% set time span
 switch flag
     case 'square'
         T_cyc=5; % 5 s
@@ -27,14 +29,16 @@ switch flag
         tspan=0:0.1:20;
 end
 
+% solve the model
 [t,yz] = ode15s(@(t,y) zener_displacement(t,y,parameters,tspan,flag),tspan,y0);
 [t,yd] = ode15s(@(t,y) dashpot_displacement(t,y,parameters,tspan,flag),tspan,0);
 
+% plot 
 create_graphs(flag,t,tspan,yz,yd,parameters);
 
 %exportgraphics(figure(1),strcat('figs/',flag,'.pdf'),'BackgroundColor','none','ContentType','vector');
 
-
+% plot separate displacements
 scale_factor=1e-12*1e6;
 figure; plot(t,scale_factor*yz,'-','LineWidth',2,'Color','#D95319')
 ylabel('Displacement  [{\mu}m]')
